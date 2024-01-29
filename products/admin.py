9ae0c5ed5945fa789ce1792b0a45b2c0482688a8
R S <rena_schott@gmx.de>
2024-01-29 11:10:39 +0000

    Added models and admin in products app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Category, Review, UserAccount, UserRating
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(UserAccount)
+admin.site.register(UserRating)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    """ 
+    Stores a single category
+    """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """ 
+    Stores a product entry
+    """
+    category = models.ManyToManyField('Category')
+    sku = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    material = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    """ 
+    Stores a review for a specific product entry
+    """
+    name = models.CharField(max_length=254)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    author = models.ForeignKey('UserAccount', on_delete=models.RESTRICT, related_name="author")
+
+    def __str__(self):
+        return self.review
+
+
+class UserAccount(models.Model):
+    """ 
+    Stores a single user profile
+    """
+    user_name = models.CharField("person's user name", max_length=30)
+    first_name = models.CharField("person's first name", max_length=30)
+    last_name =  models.CharField("person's last name", max_length=30)
+    email = models.EmailField(max_length = 254)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user_name
+
+
+class UserRating(models.Model):
+    """ 
+    Stores a rating for a specific product entry
+    """
+    author = models.ForeignKey('UserAccount', on_delete=models.RESTRICT)
+    rating = {
+        0: "0",
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5"
+    }
+    review = models.ForeignKey('Review', on_delete=models.CASCADE)
+    help_text = "Please rate the product from  0 = horrible to 5 = fantastic."
+
+    def __str__(self):
+        return self.rating
+    
