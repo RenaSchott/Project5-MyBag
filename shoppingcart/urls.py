ee1aede59677dd3a4e943a381356f06f6ef3e7a6
R S <rena_schott@gmx.de>
2024-02-05 16:22:59 +0000

    Added first part of the logic of adding products to the shopping cart

--- shoppingcart/urls.py
+++ shoppingcart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_shoppingcart, name='view_shoppingcart'),
+    path('add/<item_id>', views.add_to_shoppingcart, name='add_to_shoppingcart'),
 ]
--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -7,3 +7,20 @@ def view_shoppingcart(request):
     """ A view that renders the shopping cart """
 
     return render(request, 'shoppingcart/shoppingcart.html')
+
+
+def add_to_shoppingcart(request, item_id):
+    """ Add a quantity of the specified product to the shoppingcart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    shoppingcart = request.session.get('shoppingcart', {})
+
+    if item_id in list(shoppingcart.keys()):
+        shoppingcart[item_id] += quantity
+    else:
+        shoppingcart[item_id] = quantity
+
+    request.session['shoppingcart'] = shoppingcart
+    return redirect(redirect_url)
+    
