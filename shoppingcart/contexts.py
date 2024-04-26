1644ad852032df52d8cbaa7ccf7600fd6463d28d
R S <rena_schott@gmx.de>
2024-04-26 13:59:24 +0000

    Added shoppingcart infos to context processor

--- shoppingcart/contexts.py
+++ shoppingcart/contexts.py
@@ -1,11 +1,24 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def shoppingcart_contents(request):
 
-    bag_items = []
+    shoppingcart_items = []
     total = 0
     product_count = 0
+    shoppingcart = request.session.get('shoppingcart', {})
+
+    for item_id, quantity in shoppingcart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        shoppingcart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -17,7 +30,7 @@ def shoppingcart_contents(request):
     grand_total = delivery + total
 
     context = {
-        'bag_items': bag_items,
+        'shoppingcart_items': shoppingcart_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
