b023cb3431ad5c6fee390b39b393867c07011640
Rena Schott <rena_schott@gmx.de>
2024-07-18 09:41:52 +0000

    feat: added plain privacy policy file

--- mybag/views.py
+++ mybag/views.py
@@ -4,3 +4,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def privacy(request):
+    """
+    A view to retun the privacy policy page
+    """
+    return render(request, 'home/privacy.html')
