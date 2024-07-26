5da96639c0c0d21a437003588deb2495fbee3ba3
Rena Schott <rena_schott@gmx.de>
2024-07-26 12:59:18 +0000

    feat: added error pages

--- p5/urls.py
+++ p5/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -29,3 +30,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('forum/', include('forum.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'mybag.views.handler404'
+handler404 = 'mybag.views.handler500'
