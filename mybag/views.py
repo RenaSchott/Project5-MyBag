9e8b4f14819d11e691643b99923003ee833f6730
Rena Schott <rena_schott@gmx.de>
2024-07-26 13:17:21 +0000

    fix: fixed error pages

--- mybag/views.py
+++ mybag/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 
+
 def index(request):
     """ A view to return the index page """
 
@@ -19,17 +20,3 @@ def privacy(request):
     A view to retun the privacy policy page
     """
     return render(request, 'home/privacy.html')
-
-
-def handler404(request, exception):
-    """
-    Custom 404 page
-    """
-    return render(request, "errors/404.html", status=404)
-
-
-def handler500(request):
-    """
-    Custom 500 page
-    """
-    return render(request, "errors/500.html", status=500)
