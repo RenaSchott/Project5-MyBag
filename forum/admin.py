93bc39bb03fdd4cd9f77e3e48e000d46b9a416b1
Rena Schott <rena_schott@gmx.de>
2024-07-26 10:28:17 +0000

    feat: updated admin file in forum app

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import ForumPostCategory, ForumPost, ForumComment
 
-# Register your models here.
+
+@admin.register(ForumPost)
+class ForumPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'author', 'entry_cat', 'created_on', 'content',
+        'slug', 'status', 'approved', 'short')
+    search_fields = ['title', 'author', 'entry_cat']
+    list_filter = ('status', 'author', 'created_on', 'approved')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+
+@admin.register(ForumComment)
+class ForumCommentAdmin(admin.ModelAdmin):
+    """
+    Lists fields for display in admin, fields for search,
+    field filters.
+    """
+    list_display = (
+        'content', 'created_on', 'writer', 'comment', 'updated_on', 'approved')
+    list_filter = ('comment', 'created_on', 'approved')
+    search_fields = ['comment']
+
+
+@admin.register(ForumPostCategory)
+class ForumPostCategoryAdmin(admin.ModelAdmin):
+    pass
--- forum/models.py
+++ forum/models.py
@@ -7,17 +7,13 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class ForumPostCategory(models.Model):
     """ Defines category of forum post """
     entry_cat = models.CharField(max_length=200, unique=True)
-    user = models.ForeignKey(
-        User, on_delete=models.RESTRICT, related_name="user")
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-    approved = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ["created_on", "approved"]
+    friendly_name = models.CharField(max_length=254, null=True)
 
     def __str__(self):
         return self.entry_cat
+
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class ForumPost(models.Model):
